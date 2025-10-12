d9c6b91af2d2caa00e6d33b5bd98420240886bcd
celiopires83 <74217496+celiopires83@users.noreply.github.com>
2025-10-12 14:59:03 -0300

    Add files via upload

---
+++ CartasSuperTrunfo5.c
@@ -0,0 +1,197 @@
+#include <stdio.h>
+
+int main() {
+
+    /*
+    carta, nome, area e pib sao auto explicativos.
+    p_tur 1 e 2 sao para postos turisticos.
+    pop 1 e 2 para população.
+
+    */
+
+    unsigned int carta1, carta2, p_tur1, p_tur2;
+    unsigned long int pop1, pop2;
+    char nome1[50], nome2[50], estado1[3], estado2[3];
+    double area1, area2, pib1, pib2;
+
+    //carta 01
+    printf("Vamos inserir os valores da carta 1! \n");
+
+    printf("Numero da Carta: ");
+    scanf("%d", &carta1 );
+
+    printf("Estado: ");
+    scanf("%s", estado1);
+
+    /* para a impressão de nomes compostos podemos usar na variavel de char
+    ao invés de %s
+    */
+    printf("Nome da cidade: ");
+    scanf(" %49[^\n]", nome1);
+
+    printf("Populacao: ");
+    scanf("%uld", &pop1);
+
+    printf("Area: ");
+    scanf("%lf", &area1);
+
+    printf("PIP: ");
+    scanf("%lf", &pib1);
+
+    printf("Numero de Pontos Turisticos: ");
+    scanf("%ud", &p_tur1);
+    //fim da carta 1
+
+    //carta 2
+    printf("Agora Vamos inserir os valores da carta 2! \n");
+
+    printf("Numero da Carta: ");
+    scanf("%ud", &carta2 );
+
+    printf("Estado: ");
+    scanf("%s", estado2);
+
+    printf("Nome da cidade: ");
+    scanf(" %49[^\n]", nome2);
+
+    printf("Populacao: ");
+    scanf("%ud", &pop2);
+
+    printf("Area: ");
+    scanf("%lf", &area2);
+
+    printf("PIP: ");
+    scanf("%lf", &pib2);
+
+    printf("Numero de Pontos Turisticos: ");
+    scanf("%ud", &p_tur2);
+    //fim da carta 2
+
+    double densidade1 = (double)pop1 / area1;
+    double densidade2 = (double)pop2 / area2;
+
+    double percapita1 = (double)pib1 / pop1;
+    double percapita2 = (double)pib2 / pop2;
+
+    double densidade_inverso1 = 1 / densidade1;
+    double densidade_inverso2 = 1 / densidade2;
+
+    double superpoder1 = (double) pop1 + area1 + pib1 + p_tur1 + percapita1;
+    double superpoder2 = (double) pop2 + area2 + pib2 + p_tur2 + percapita2;
+
+
+
+    //impressao
+
+    printf("\n\n Primeira Carta! \n");
+    printf("\n Carta: %d \n", carta1);
+    printf("Estado: %s \n", estado1 );
+    //codigo é a junção do numero da carta + o codigo do estado.
+    printf("Código %s%d \n", estado1, carta1);
+    printf("Nome: %s \n", nome1);
+    printf("População: %d \n", pop1);
+    printf("Area: %.2lf \n", area1);
+    printf("PIB: %.2lf \n", pib1);
+    printf("Pontos Turisticos: %d \n\n", p_tur1);
+
+    //fase 2
+    printf("Densidade: %.3lf \n", densidade1);
+    printf("Pib per capita: %.3lf \n", percapita1);
+
+    //fase 3
+    printf("Inverso da Densidade Populacional: %.4lf \n", densidade_inverso1);
+    printf("Super Poder: %.3lf \n", superpoder1);
+
+
+
+    printf("\n\n Segunda Carta! \n");
+    printf("\n Carta: %d \n", carta2);
+    printf("Estado: %s \n", estado2 );
+    //codigo é a junção do numero da carta + o codigo do estado.
+    printf("Código %s%d \n", estado2, carta2);
+    printf("Nome: %s \n", nome2);
+    printf("População: %d \n", pop2);
+    printf("Area: %.2lf \n", area2);
+    printf("PIB: %.2lf \n", pib2);
+    printf("Pontos Turisticos: %d \n\n", p_tur2);
+
+    //fase 2
+    printf("Densidade: %.3lf \n", densidade2);
+    printf("Pib per capita: %.3lf \n", percapita2);
+
+    //fase 3
+    printf("Inverso da Densidade Populacional: %.4lf \n", densidade_inverso2);
+    printf("Super Poder: %.3lf \n\n\n", superpoder2);
+
+    int menu;
+
+    printf("1 - Populacao: \n");
+    printf("2 - Area: \n");
+    printf("3 - PIB: \n");
+    printf("4 - Numero de Pontos Turisticos: \n");
+    printf("5 - Densidade Demografica: \n");
+    scanf("%d", &menu);
+
+    switch (menu) {
+        case 1:
+            if(pop1 > pop2){
+                printf("%s a campea com uma populacao de %u! \n", nome1, pop1);
+            } else {
+                if (pop2 > pop1) {
+                    printf("%s a campea com uma populacao de %u! \n", nome2, pop2);
+                } else {
+                    printf("O jogo foi empate!");
+                }
+            }
+        break;
+        case 2:
+            if(area1 > area2){
+                printf("%s a campea com uma area de %.2f \n", nome1, area1);
+            } else {
+                if (area2 > area1) {
+                    printf("%s a campea com uma area de %.2f! \n", nome2, area2);
+                } else {
+                    printf("O jogo foi empate!");
+                }
+            }
+        break;
+        case 3:
+            if(pib1 > pib2){
+                printf("%s a campea com um pib de %.2f \n", nome1, pib1);
+            } else {
+                if (pib2 > pib1) {
+                    printf("%s a campea com um pib de %.2f! \n", nome2, pib2);
+                } else {
+                    printf("O jogo foi empate!");
+                }
+            }
+        break;
+        case 4:
+            if(p_tur1 > p_tur2){
+                printf("%s a campea com um %u pontos turisticos \n", nome1, p_tur1);
+            } else {
+                if (p_tur2 > p_tur1) {
+                    printf("%s a campea com %u pontos turisticos! \n", nome2, p_tur2);
+                } else {
+                    printf("O jogo foi empate!");
+                }
+            }
+        break;
+        case 5:
+            if(densidade_inverso1 < densidade_inverso2){
+                printf("%s a campea com uma densidade demografica de %.2f \n", nome1, densidade_inverso1);
+            } else {
+                if (densidade_inverso1 > densidade_inverso2) {
+                    printf("%s a campea com uma densidade demografica de %.2f \n", nome1, densidade_inverso2);
+                } else {
+                    printf("O jogo foi empate!");
+                }
+            }
+        break;
+        default:
+            printf("Opcao invalida! \n");
+
+    }
+
+    return 0;
+}
